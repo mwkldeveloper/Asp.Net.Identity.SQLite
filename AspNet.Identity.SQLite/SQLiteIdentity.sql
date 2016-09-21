30c6d7f3ed7c89a7474f42e3492ea12275108123
leung85 <mwkleung85dev@gmail.com>
2016-09-21 14:58:01 +0800

    init commit
    add src

---
+++ AspNet.Identity.SQLite/SQLiteIdentity.sql
@@ -0,0 +1,54 @@
+﻿
+CREATE TABLE IF NOT EXISTS 'AspNetRoles' ( 
+  Id varchar(128) NOT NULL,
+  Name varchar(256) NOT NULL,
+  PRIMARY KEY (Id)
+);
+
+CREATE TABLE IF NOT EXISTS 'AspNetUsers' (
+ Id varchar (128) NOT NULL,
+ Email varchar (256) DEFAULT NULL,
+ EmailConfirmed tinyint (1) NOT NULL,
+ PasswordHash longtext,
+ SecurityStamp longtext,
+ PhoneNumber longtext,
+ PhoneNumberConfirmed tinyint (1) NOT NULL,
+ TwoFactorEnabled tinyint (1) NOT NULL,
+ LockoutEndDateUtc datetime DEFAULT NULL,
+ LockoutEnabled tinyint (1) NOT NULL,
+ AccessFailedCount int (11) NOT NULL,
+ UserName varchar (256) NOT NULL,
+ IsActivated tinyint(1) NOT NULL DEFAULT (0),
+ PRIMARY KEY (Id));
+
+CREATE TABLE IF NOT EXISTS 'AspNetUserClaims' ( 
+  Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  ClaimType varchar(256) NULL,
+  ClaimValue varchar(256) NULL,
+  UserId varchar(128) NOT NULL,
+  FOREIGN KEY (UserId) REFERENCES AspNetUsers (Id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS 'AspNetUserLogins' ( 
+  UserId varchar(128) NOT NULL,
+  LoginProvider varchar(128) NOT NULL,
+  ProviderKey varchar(128) NOT NULL,
+  PRIMARY KEY (UserId, LoginProvider, ProviderKey),
+  FOREIGN KEY (UserId) REFERENCES AspNetUsers (Id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS 'AspNetUserRoles' ( 
+  UserId varchar(128) NOT NULL,
+  RoleId varchar(128) NOT NULL,
+  PRIMARY KEY (UserId, RoleId),
+  FOREIGN KEY (RoleId) REFERENCES AspNetRoles (Id) ON DELETE CASCADE,
+  FOREIGN KEY (UserId) REFERENCES AspNetUsers (Id) ON DELETE CASCADE
+);
+
+CREATE INDEX IX_AspNetUserClaims_UserId ON AspNetUserClaims (UserId);
+
+CREATE INDEX IX_AspNetUserLogins_UserId ON AspNetUserLogins (UserId);
+
+CREATE INDEX IX_AspNetUserRoles_RoleId ON AspNetUserRoles (RoleId);
+
+CREATE INDEX IX_AspNetUserRoles_UserId ON AspNetUserRoles (UserId);
---
+++ AspNet.Identity.SQLite/UserRoleTable.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace AspNet.Identity.MySQL
+{
+    /// <summary>
+    /// Class that represents the AspNetUserRoles table in the MySQL Database
+    /// </summary>
+    public class UserRolesTable
+    {
+        private SQLiteDatabase _database;
+
+        /// <summary>
+        /// Constructor that takes a SQLiteDatabase instance 
+        /// </summary>
+        /// <param name="database"></param>
+        public UserRolesTable(SQLiteDatabase database)
+        {
+            _database = database;
+        }
+
+        /// <summary>
+        /// Returns a list of user's roles
+        /// </summary>
+        /// <param name="userId">The user's id</param>
+        /// <returns></returns>
+        public List<string> FindByUserId(string userId)
+        {
+            List<string> roles = new List<string>();
+            string commandText = "Select AspNetRoles.Name from AspNetUserRoles, AspNetRoles where AspNetUserRoles.UserId = @userId and AspNetUserRoles.RoleId = AspNetRoles.Id";
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("@userId", userId);
+
+            var rows = _database.Query(commandText, parameters);
+            foreach(var row in rows)
+            {
+                roles.Add(row["Name"]);
+            }
+
+            return roles;
+        }
+
+        /// <summary>
+        /// Deletes all roles from a user in the AspNetUserRoles table
+        /// </summary>
+        /// <param name="userId">The user's id</param>
+        /// <returns></returns>
+        public int Delete(string userId)
+        {
+            string commandText = "Delete from AspNetUserRoles where UserId = @userId";
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("UserId", userId);
+
+            return _database.Execute(commandText, parameters);
+        }
+
+        /// <summary>
+        /// Inserts a new role for a user in the AspNetUserRoles table
+        /// </summary>
+        /// <param name="user">The User</param>
+        /// <param name="roleId">The Role's id</param>
+        /// <returns></returns>
+        public int Insert(IdentityUser user, string roleId)
+        {
+            string commandText = "Insert into AspNetUserRoles (UserId, RoleId) values (@userId, @roleId)";
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("userId", user.Id);
+            parameters.Add("roleId", roleId);
+
+            return _database.Execute(commandText, parameters);
+        }
+    }
+}
