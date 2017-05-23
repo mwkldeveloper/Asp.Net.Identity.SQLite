a356e01a7baec784424ddaf3e0764afa919503db
Marcus <Marcus@DESKTOP-A0FN0P7>
2017-05-23 19:58:59 +0800

    update nuget packge

--- AspNet.Identity.SQLite/Properties/AssemblyInfo.cs
+++ AspNet.Identity.SQLite/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AspNet.Identity.SQLite")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SQLite Provider for Asp.net Identity")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspNet.Identity.SQLite")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
